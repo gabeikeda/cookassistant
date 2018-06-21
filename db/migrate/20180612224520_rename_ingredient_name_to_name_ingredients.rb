2a7fd9c5e908c646b3f9f8e37c978c50e591e11d
Gabe <gabeikeda@gmail.com>
2018-06-21 22:45:10 +0000

    ingredient column

--- db/migrate/20180612224520_rename_ingredient_name_to_name_ingredients.rb
+++ db/migrate/20180612224520_rename_ingredient_name_to_name_ingredients.rb
@@ -1,5 +1,9 @@
 class RenameIngredientNameToNameIngredients < ActiveRecord::Migration[5.2]
-  def change
+  def up
     rename_column :ingredients, :ingredient_name, :name
+  end
+
+  def down
+    rename_column :ingredients, :name, :ingredient_name
   end
 end
